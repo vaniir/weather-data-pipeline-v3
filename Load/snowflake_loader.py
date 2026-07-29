05dc54786355d0cf020701dd77d017f49db4e68a
Matias <matiasarvin214@gmail.com>
2026-07-30 02:32:54 +0800

    refactored into function

--- Load/snowflake_loader.py
+++ Load/snowflake_loader.py
@@ -2,28 +2,26 @@ from config import settings
 import snowflake.connector
 import json
 
-payload = {
-    "hellow" : "world",
-    "test": "succeed"
-}
+def get_connection():
+    return snowflake.connector.connect(
+        user=settings.SNOWFLAKE_USER,
+        password=settings.SNOWFLAKE_PASSWORD,
+        account=settings.SNOWFLAKE_ACCOUNT,
+        warehouse=settings.SNOWFLAKE_WAREHOUSE,
+        database=settings.SNOWFLAKE_DATABASE,
+        schema=settings.SNOWFLAKE_SCHEMA
+    )
 
-conn = snowflake.connector.connect(
-    user=settings.SNOWFLAKE_USER,
-    password=settings.SNOWFLAKE_PASSWORD,
-    account=settings.SNOWFLAKE_ACCOUNT,
-    warehouse=settings.SNOWFLAKE_WAREHOUSE,
-    database=settings.SNOWFLAKE_DATABASE,
-    schema=settings.SNOWFLAKE_SCHEMA
-)
+def load_to_snowflake(table_name: str, payload: dict):
+    conn = get_connection()
+    cursor = conn.cursor()
 
-cursor = conn.cursor()
+    cursor.execute(f"""
+        INSERT INTO {table_name} (payload)
+        SELECT PARSE_JSON(%s)
+    """,(json.dumps(payload),)
+    )
 
-cursor.execute("""
-    INSERT INTO OPENWEATHER_RAW (payload)
-    SELECT PARSE_JSON(%s)
-""",(json.dumps(payload),)
-)
-conn.commit()
-
-cursor.close()
-conn.close()
+    conn.commit()
+    cursor.close()
+    conn.close()
